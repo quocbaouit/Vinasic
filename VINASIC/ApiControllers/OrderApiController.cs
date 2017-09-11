b1ea47253fc216aaf3f19c8a661e990de69b29ba
BaoTran <bao.tran@newoceaninfosys.com>
2017-09-11 09:59:39 +0700

    #add api Controler

---
+++ VINASIC/ApiControllers/OrderApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+namespace MCMContactManagement.API.Controllers
+{
+    [AllowAnonymous]
+    public class OrderApiController : ApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public OrderApiController()
+        {
+        }
+        [HttpGet]
+        public string Get(string id)
+        {
+            return "Test";
+        }
+        [HttpGet]
+        [ActionName("GetById")]
+        public string GetById(string Id)
+        {
+            return "Test";
+        }
+    }
+}
+
--- VINASIC/App_Start/WebApiConfig.cs
+++ VINASIC/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace VINASIC
 {
@@ -9,6 +8,39 @@ namespace VINASIC
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Routes.MapHttpRoute(
+                name: "ApiWithActionGet",
+                routeTemplate: "api/{controller}/{id}/{action}",
+                defaults: new { action = "Get" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiWithActionPost",
+                routeTemplate: "api/{controller}/{id}/{action}",
+                defaults: new { action = "Post" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Post) }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiWithActionPut",
+                routeTemplate: "api/{controller}/{id}/{action}",
+                defaults: new { action = "Put" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Put) }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiWithActionDelete",
+                routeTemplate: "api/{controller}/{id}/{action}",
+                defaults: new { action = "Delete" },
+                constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Delete) }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiWithAction",
+                routeTemplate: "api/{controller}/{id}/{action}"
+            );
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
