bc7417a214fd27009b33cd1d642a8ce30a1a6bc0
BaoTran <quocbao.uit@gmail.com>
2020-05-14 08:09:50 +0700

    #sticker mockup.

--- VINASIC/App_Start/RouteConfig.cs
+++ VINASIC/App_Start/RouteConfig.cs
@@ -7,10 +7,46 @@ using System.Web.Routing;
 
 namespace VINASIC
 {
+    public class SubdomainRoute : RouteBase
+    {
+
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            var host = httpContext.Request.Url.Host;
+            var index = host.IndexOf(".");
+            string[] segments = httpContext.Request.Url.PathAndQuery.Split('.');
+
+            if (index < 0)
+                return null;
+
+            var subdomain = host.Substring(0, index);
+            string controller = (segments.Length > 0) ? segments[0] : "DashBoard";
+            string action = (segments.Length > 1) ? segments[1] : "Index";
+
+            if (subdomain.Contains("baogia"))
+            {
+                controller = "Sticker";
+                action = "Index";
+            }
+            var routeData = new RouteData(this, new MvcRouteHandler());
+
+            routeData.Values.Add("controller", controller); //Goes to the relevant Controller  class
+            routeData.Values.Add("action", action); //Goes to the relevant action method on the specified Controller
+            //routeData.Values.Add("subdomain", subdomain); //pass subdomain as argument to action method
+            return routeData;
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            //Implement your formating Url formating here
+            return null;
+        }
+    }
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.Add(new SubdomainRoute());
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             
 
---
+++ VINASIC/Controllers/StickerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VINASIC.Controllers
+{
+    public class StickerController : Controller
+    {
+        //
+        // GET: /Sticker/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
