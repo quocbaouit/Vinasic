274e3cbae48bc859a89a5efc2ab07ce77a8b6c27
BaoTran <quocbao.uit@gmail.com>
2021-03-15 16:23:11 +0700

    #add color code.

--- VINASIC.Object/T_Order.cs
+++ VINASIC.Object/T_Order.cs
@@ -55,6 +55,7 @@ namespace VINASIC.Object
         public string StatusName { get; set; }
         public Nullable<int> Approval { get; set; }
         public string ReasonApproval { get; set; }
+        public string ColorCode { get; set; }
     
         public virtual T_Customer T_Customer { get; set; }
         public virtual T_User T_User { get; set; }
--- VINASIC/App_Start/RouteConfig.cs
+++ VINASIC/App_Start/RouteConfig.cs
@@ -53,7 +53,7 @@ namespace VINASIC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Sticker", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Order", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
